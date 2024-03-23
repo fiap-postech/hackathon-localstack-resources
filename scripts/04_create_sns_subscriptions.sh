0b7bac7d9e2c2ed75ae4f781c370339deb53ad83
Antonio Miranda <antoniogabrielti@gmail.com>
2024-03-23 02:22:54 -0300

    fix: ajuste de chmod para script do s3 bucket

--- scripts/04_create_sns_subscriptions.sh
+++ scripts/04_create_sns_subscriptions.sh
@@ -2,4 +2,4 @@
 
 echo "########### Creating sns subscriptions ###########"
 aws --endpoint-url=http://localhost:4566 sns subscribe --profile=localstack --topic-arn arn:aws:sns:us-east-1:000000000000:local-time-tracking-event-topic --protocol sqs --notification-endpoint arn:aws:sqs:us-east-1:000000000000:local-hackathon-time-sheet-time-tracking-event-queue --attributes RawMessageDelivery=true
-sudo chmod +x /etc/localstack/init/ready.d/05_create_s3.sh
+chmod +x /etc/localstack/init/ready.d/05_create_s3.sh
